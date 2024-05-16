f5a733f467bb96598a37b6c469f58c5bfeda0ee9
Lelouch223311 <foracademy228@mail.ru>
2024-05-16 20:30:05 +0300

    Добавлены .env для Деплоя и Разработки,не принимаются картинки с порта 1337

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,15 @@ import './style.css'
 import App from './App.vue'
 import router from './router/index.js'
 
+
+// main.js
+// import { dotenv } from 'dotenv'
+// require('dotenv').config()
+
+// console.log(process.env.HOST)
+
+
+
 const app = createApp(App)
 app.use(router)
 
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// const dotenv = require('dotenv');
+// import { dotenv } from 'dotenv' dotenv(),
+
+// import pkg from 'file:///D:/AWeb34/DiplomDone/client/node_modules/dotenv/lib/main.js';
+// const { dotenv } = pkg;
+
+// const require = require('esm')(module)
+// const dotenv = require('dotenv')
+
+// dotenv.config()
+
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
