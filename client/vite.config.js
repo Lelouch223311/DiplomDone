729b13900bbd7b7b3b3f40ef662499548729c344
Lelouch223311 <foracademy228@mail.ru>
2024-05-23 19:24:10 +0300

    server/config/server.js added ip and port

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,45 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// const dotenv = require('dotenv');
-// import { dotenv } from 'dotenv' dotenv(),
-
-// import pkg from 'file:///D:/AWeb34/DiplomDone/client/node_modules/dotenv/lib/main.js';
-// const { dotenv } = pkg;
-
-// const require = require('esm')(module)
-// const dotenv = require('dotenv')
-
-// dotenv.config()
-
-
-
+import { defineConfig } from 'vite';
 import dotenv from 'dotenv';
-// const dotenvExpand = require('dotenv-expand');
 import dotenvExpand from 'dotenv-expand';
-
-const myEnv = dotenv.config();
-dotenvExpand(myEnv);
-
-console.log(dotenvExpand);
-
-// import dotenvExpand from 'dotenv-expand'; // added dotenvExpand
 import { join } from 'path';
-// import { webpack } from 'dotenv-webpack';
-// import pkg from 'dotenv-webpack';
-// const webpack = pkg;
+import { webpack } from 'dotenv-webpack';
 
+const env = dotenv.config({ path: join(__dirname, '.env') });
+dotenvExpand(env);
 
-// const env = dotenv.config({ path: join(__dirname, '.env') });
-// dotenvExpand(env);
-
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: 
-  [vue(),
+  plugins: [
     webpack({
       path: join(__dirname, '.env')
     })
-  ],
-})
+  ]
+});
