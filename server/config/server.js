4c4a4e36d1069b8054991d8bf5757e0b004bce34
Lelouch223311 <foracademy228@mail.ru>
2024-05-23 19:26:17 +0300

    Swap to standart server.js in server/config

--- server/config/server.js
+++ server/config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '37.18.102.197'),
+  host: env('HOST', 'localhost'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
